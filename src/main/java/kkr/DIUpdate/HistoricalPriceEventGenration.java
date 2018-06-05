64e34d6df3ec36b9ebea93cd2ac4ae9f1192335d
sislam978 <sislam@lrglobalbd.com>
2018-06-05 15:58:07 +0600

    2018-06-05

--- src/main/java/kkr/DIUpdate/HistoricalPriceEventGenration.java
+++ src/main/java/kkr/DIUpdate/HistoricalPriceEventGenration.java
@@ -6,39 +6,49 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
+
+
 
 import kkr.DIUpdate.CommonUtils.DataBaseUtils;
 import kkr.DIUpdate.CommonUtils.DateUtils;
 
 public class HistoricalPriceEventGenration {
-	
-	public static void main(String args[]) throws ClassNotFoundException, SQLException{
-		
-		String sql_query="SELECT * FROM `volatility_index` ORDER BY history_date";
-		Connection con=DataBaseUtils.connectLocal();
-		
-		Statement kkrDb=con.createStatement();
-		ResultSet rs = kkrDb.executeQuery(sql_query);
-		Map<String,String> eventDateMap= new HashMap<String, String>();
-		String start_date=null;
-		String end_date=null;
-		String prev_date=null;
-		int i=0;
-		while(rs.next()){
-			if(rs.getObject(8)==null) continue;
-			String d_d=rs.getString(2);
-			
-			if(rs.getDouble(8)>0.1){
-				if(i==0){
-					start_date=rs.getString(2);
-					i++;
-				}
-				
-				prev_date=rs.getString(2);
 
+	public static void main(String args[]) throws Exception {
+
+		Connection con = DataBaseUtils.connectLocal();
+		Connection conKKrProd=DataBaseUtils.connectkkrProd();
+		VolatilityEventCreate(con);
+		TreasuryEventCreate(con);
+		
+		System.out.println("enter a date where to start calculating the spy:");
+		Scanner input=new Scanner(System.in);
+		String d_date=input.nextLine();
+	}
+
+	public static void SpyEventCreate(Connection conKkr,String Desired_date,Connection con) throws Exception{
+		String SQL_query = "SELECT PRICE_DATE FROM `kkr_price` WHERE PRICE_DATE>='"+Desired_date+"' ORDER BY PRICE_DATE ASC";
+		Statement locStat = conKkr.createStatement();
+
+		ResultSet rsL = locStat.executeQuery(SQL_query);
+		String start_date = null;
+		String end_date = null;
+		String prev_date = null;
+		int i = 0;
+		Map<String, String> eventDateMap = new HashMap<String, String>();
+		while (rsL.next()) {
+			if(i==0){
+				start_date=rsL.getString(1);
+				i++;
+			}
+			double vv=spyEventCreateProcess(conKkr,rsL.getString(1));
+			if(vv<0.1){
+				prev_date=rsL.getString(1);
 			}
 			else{
 				end_date=prev_date;
@@ -46,48 +56,160 @@ public class HistoricalPriceEventGenration {
 				i=0;
 			}
 		}
-		TreeMap<String, String> sorted = new TreeMap<>(eventDateMap);
-
-		// Copy all data from hashMap into TreeMap
-		//sorted.putAll(eventDateMap);
-		InsertNewEvent(con,sorted);
 		
+		InsertNewEvent(con,eventDateMap,"S&P Down");
+			
+	}
+	private static double spyEventCreateProcess(Connection conKkr, String desired_date) throws SQLException {
+		// TODO Auto-generated method stub
+		String SQL_QUERY = "SELECT PRICE_CLOSE, PRICE_DATE FROM kkr_price where PRICE_DATE<='"+desired_date+"' ORDER BY PRICE_DATE DESC LIMIT 23";
+		
+		Statement statement = conKkr.createStatement();
+		
+		ResultSet rs = statement.executeQuery(SQL_QUERY);
+		ArrayList<Double> closeList=new ArrayList<Double>();
+		double oriClose=-1;
+		while(rs.next()){
+			if(rs.getString(2).equals(desired_date)){
+				oriClose=rs.getDouble(1);
+			}
+			closeList.add(rs.getDouble(1));
+		}
+		Collections.sort(closeList);
+		double vv=oriClose/closeList.get(closeList.size()-1)-1;
+		return vv;
 	}
 
-	private static void InsertNewEvent(Connection con,Map<String, String> eventDateMap) throws SQLException {
+	public static void TreasuryEventCreate(Connection con) throws Exception{
+		String sql_query = "SELECT * FROM `treasury_yield_curve_rates` ORDER BY rates_date";
+		
+
+		Statement kkrDb = con.createStatement();
+		ResultSet rs = kkrDb.executeQuery(sql_query);
+		Map<String, String> eventDateMap = new HashMap<String, String>();
+		String start_date = null;
+		String end_date = null;
+		String prev_date = null;
+		int i = 0;
+		
+		while (rs.next()) {
+			
+			if (rs.getObject(6) == null || rs.getObject(11) == null)
+				continue;
+			double yr1=rs.getDouble(6);
+			double yr10=rs.getDouble(11);
+
+			double diff=yr1-yr10;
+			if (diff < 0) {
+				if (i == 0) {
+					start_date = rs.getString(2);
+					i++;
+				}
+
+				prev_date = rs.getString(2);
+
+			} else {
+				end_date = prev_date;
+				eventDateMap.put(start_date, end_date);
+				i = 0;
+			}
+		}
+		TreeMap<String, String> sorted = new TreeMap<>(eventDateMap);
+		// Copy all data from hashMap into TreeMap
+		// sorted.putAll(eventDateMap);
+		InsertNewEvent(con, sorted,"Treasury Inverse");
+	}
+	public static void VolatilityEventCreate(Connection con) throws Exception {
+		
+		String sql_query = "SELECT * FROM `volatility_index` ORDER BY history_date";
+		
+
+		Statement kkrDb = con.createStatement();
+		ResultSet rs = kkrDb.executeQuery(sql_query);
+		Map<String, String> eventDateMap = new HashMap<String, String>();
+		String start_date = null;
+		String end_date = null;
+		String prev_date = null;
+		int i = 0;
+		while (rs.next()) {
+			if (rs.getObject(8) == null)
+				continue;
+			String d_d = rs.getString(2);
+
+			if (rs.getDouble(8) > 0.1) {
+				if (i == 0) {
+					start_date = rs.getString(2);
+					i++;
+				}
+
+				prev_date = rs.getString(2);
+
+			} else {
+				end_date = prev_date;
+				eventDateMap.put(start_date, end_date);
+				i = 0;
+			}
+		}
+		TreeMap<String, String> sorted = new TreeMap<>(eventDateMap);
+		// Copy all data from hashMap into TreeMap
+		// sorted.putAll(eventDateMap);
+		InsertNewEvent(con, sorted,"Volatility Spike");
+	}
+
+	private static void InsertNewEvent(Connection con, Map<String, String> eventDateMap,String event_type) throws Exception {
 		// TODO Auto-generated method stub
-		for(Map.Entry<String,String>entry:eventDateMap.entrySet()){
-			String sql_query="SELECT * FROM `historical_price_event` where event_type='"+"Volatility Spike"+"' AND "
-					+ "start_date='"+entry.getKey()+"' and end_date='"+entry.getValue()+"'";
+		for (Map.Entry<String, String> entry : eventDateMap.entrySet()) {
+			String sql_query = "SELECT * FROM `historical_price_event` where event_type='" + event_type
+					+ "' AND " + "start_date='" + entry.getKey() + "' and end_date='" + entry.getValue() + "'";
 			int size = 0;
-			Statement ss=con.createStatement();
-			ResultSet rs=ss.executeQuery(sql_query);
+			Statement ss = con.createStatement();
+			ResultSet rs = ss.executeQuery(sql_query);
 			if (rs != null) {
 				rs.beforeFirst();
 				rs.last();
 				size = rs.getRow();
 			}
-			if(size<1){
-				Statement event_name=con.createStatement();
-				ResultSet eve=event_name.executeQuery("SELECT event_name FROM `historical_price_event` where "
-						+ "event_type='"+"Volatility Spike"+"' order by historical_price_event_id desc limit 1");
-				eve.next();
+			if (size < 1) {
+				String Sql_stat=null;
+				String final_name = null;
 				PreparedStatement pSLocal = con.prepareStatement(
 						"insert into  historical_price_event (event_name,event_type,start_date,end_date) values (?,?,?,?)");
-				String []event=eve.getString(1).split(" ");
-				int vv=Integer.parseInt(event[2]);
-				vv++;
 				
-				String final_name=null;
-				if(vv<100){
-					final_name=event[0]+" "+event[1]+" 0"+vv;
+				if(event_type.equals("Volatility Spike")){
+					Sql_stat="SELECT event_name FROM `historical_price_event` where event_type='" + event_type + "' order by historical_price_event_id desc limit 1";
+					Statement event_name = con.createStatement();
+					ResultSet eve = event_name.executeQuery(Sql_stat);
+					eve.next();
+					String[] event = eve.getString(1).split(" ");
+					int vv = Integer.parseInt(event[2]);
+					vv++;
+					if (vv < 100) {
+						final_name = event[0] + " " + event[1] + " 0" + vv;
+					} else {
+						final_name = event[0] + " " + event[1] + " " + vv;
+					}
 				}
-				else{
-					final_name=event[0]+" "+event[1]+" "+vv;
+				else if(event_type.equals("Treasury Inverse")){
+					String d_date=DateUtils.stringTodate(entry.getKey(), "yyyy-MM-dd","MMM d, yyyy");
+					String d_array[]=d_date.split(",|\\s");
+					final_name=event_type+d_array[0]+d_array[3];
+				}
+				else if(event_type.equals("S&P Down")){
+					Sql_stat="SELECT event_name FROM `historical_price_event` where event_type='" + event_type + "' order by historical_price_event_id desc limit 1";
+					Statement event_name = con.createStatement();
+					ResultSet eve = event_name.executeQuery(Sql_stat);
+					eve.next();
+					String[] event = eve.getString(1).split(" ");
+					int vv = Integer.parseInt(event[2]);
+					vv++;
+					if (vv < 10) {
+						final_name = event[0] + " 0"+ vv;
+					} else {
+						final_name = event[0] + " " + vv;
+					}
 				}
 				pSLocal.setString(1, final_name);
-				
-				pSLocal.setString(2, "Volatility Spike");
+				pSLocal.setString(2, event_type);
 				pSLocal.setString(3, entry.getKey());
 				pSLocal.setString(4, entry.getValue());
 				pSLocal.execute();
